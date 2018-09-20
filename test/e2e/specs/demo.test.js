f920a40c2608daa2c2dc4e67d3d0a783d8744c19
Nicola Heald <nicola@notnowlewis.com>
2018-09-20 11:03:11 +0100

    Attempt 3 - full embed response with html taken out

--- test/e2e/specs/demo.test.js
+++ test/e2e/specs/demo.test.js
@@ -3,8 +3,28 @@
  */
 import { visitAdmin } from '../support/utils';
 
+// The response from Vimeo, but with the iframe taken out.
 const MOCK_EMBED_RESPONSE = {
-	html: '<p>Embed response.</p>',
+	type: 'video',
+	version: '1.0',
+	provider_name: 'Vimeo',
+	provider_url: 'https://vimeo.com/',
+	title: 'The Mountain',
+	author_name: 'TSO Photography',
+	author_url: 'https://vimeo.com/terjes',
+	is_plus: '0',
+	account_type: 'basic',
+	html: '<p>Embedded content</p>',
+	width: 600,
+	height: 338,
+	duration: 189,
+	thumbnail_url: 'https://i.vimeocdn.com/video/145026168_295x166.jpg',
+	thumbnail_width: 295,
+	thumbnail_height: 166,
+	thumbnail_url_with_play_button: 'https://i.vimeocdn.com/filter/overlay?src0=https%3A%2F%2Fi.vimeocdn.com%2Fvideo%2F145026168_295x166.jpg&src1=http%3A%2F%2Ff.vimeocdn.com%2Fp%2Fimages%2Fcrawler_play.png',
+	upload_date: '2011-04-15 08:35:35',
+	video_id: 22439234,
+	uri: '/videos/22439234',
 };
 
 describe( 'new editor state', () => {
