bead6ccacd291e37b1710f7967b583356c94656a
Nicola Heald <nicola@notnowlewis.com>
2018-09-20 10:15:28 +0100

    Attempt 1 to mock the embed response

--- test/e2e/specs/demo.test.js
+++ test/e2e/specs/demo.test.js
@@ -3,8 +3,25 @@
  */
 import { visitAdmin } from '../support/utils';
 
+const MOCK_EMBED_RESPONSE = {
+	html: '<p>Embed response.</p>',
+};
+
 describe( 'new editor state', () => {
 	beforeAll( async () => {
+		// Intercept embed requests so that scripts loaded from third parties
+		// cannot leave errors in the console and cause the test to fail.
+		await page.setRequestInterceptionEnabled( true );
+		page.on( 'request', ( request ) => {
+			if ( request.url === '/index.php?rest_route=/oembed/1.0/proxy&url=https%3A%2F%2Fvimeo.com%2F22439234' ) {
+				request.respond( {
+					content: 'application/json',
+					body: JSON.stringify( MOCK_EMBED_RESPONSE ),
+				} );
+			} else {
+				request.continue();
+			}
+		} );
 		await visitAdmin( 'post-new.php', 'gutenberg-demo' );
 	} );
 
